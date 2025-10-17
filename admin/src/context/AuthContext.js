36fbb832759b16bc0bb28ee753f6829642b150f1
Phan Tien Manh <manhamsterdam2003@gmail.com>
2025-10-17 15:42:32 +0700

    fix all login bugs

--- admin/src/context/AuthContext.js
+++ admin/src/context/AuthContext.js
@@ -1,5 +1,5 @@
-import { createContext, useContext, useState } from "react";
-
+import { createContext, useContext, useState, useEffect } from "react";
+import Client from "../api/Client";
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
@@ -9,22 +9,62 @@ export const AuthProvider = ({ children }) => {
     return saved ? JSON.parse(saved) : null;
   });
 
-  // Hàm login — lưu user vào state và localStorage , token lưu localStorage
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // biến isLoggedIn này để duy trì trạng thái đăng nhập, tránh back lại trang login khi đã đăng nhập
+  
+  const token = localStorage.getItem("token");
+  const [loading, setLoading] = useState(true);
+  // loading dùng để biểu thị app đang trong quá trình kiểm tra token xác thực, nên chưa thể biết isLoggedIn 
+  // là true hay false, tránh việc truyền token cũ xuống các component con trước khi xác thực xong hoặc bị sửa token mà vẫn đăng nhập dc 
+
+
+  useEffect(() => {
+    const verifyToken = async () => {
+      if (!token) {
+        setIsLoggedIn(false);
+        setLoading(false);
+        return;
+      }
+      try {
+        // Gọi API xác minh token
+        const res = await Client.get("auth/checkme", {
+          headers: { Authorization: `Bearer ${token}` },
+        });
+
+        // Nếu hợp lệ thì cập nhật user và login state
+        setUser(res.user);
+        setIsLoggedIn(true);
+      } catch (err) {
+        // Token sai hoặc hết hạn
+        console.error("Token không hợp lệ:", err);
+        localStorage.removeItem("token");
+        localStorage.removeItem("user");
+        setUser(null);
+        setIsLoggedIn(false);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    verifyToken();
+  }, [token]);
+
   const login = (userData, token) => {
     setUser(userData);
     localStorage.setItem("user", JSON.stringify(userData));
     localStorage.setItem("token", token);
+    setIsLoggedIn(true);
   };
-  
-  // hàm logout
+
   const logout = () => {
     setUser(null);
-    localStorage.clear();
+    setIsLoggedIn(false);
+    localStorage.removeItem("user");
+    localStorage.removeItem("token");
   };
 
   return (
-    <AuthContext.Provider value={{ user, login, logout }}>
-      {children}
+    <AuthContext.Provider value={{ user, token, login, logout, isLoggedIn, loading }}>
+      {!loading && children}
     </AuthContext.Provider>
   );
 };
